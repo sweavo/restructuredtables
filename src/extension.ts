1d14e1d15444fe5dfebb1eb4c9aaa6a5f428f902
Steve Carter <sweavo@gmail.com>
2021-03-14 11:17:11 +0000

    a command, that just writes a message, and the start of code to extract a gridtable from the current cursor

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cals from './cals';
+import * as ReST from './ReST';
+
+
+function isTableLine( line: string) : boolean {
+	return (line.match(/^\s*[|+]/))?true:false;
+}
+
+// Given a vscode document, and a line on which the cursor appears, return the first and last+1
+// lines of the table. If first == last then there was no table found.
+function findTableRange( document: vscode.TextDocument, startLine: number  ) {
+	let start = startLine;
+	let end = startLine;
+	const maxLine = document.lineCount;
+	
+	for ( let cursor=start; cursor>=0 && isTableLine( document.lineAt(cursor).text); --cursor) {
+		start=cursor;		
+	}
+
+	for ( let cursor=start; cursor<=maxLine && isTableLine( document.lineAt(cursor).text); ++cursor) {
+		end=cursor;		
+	}
+
+	return new vscode.Range( 
+				new vscode.Position(start, 0),
+				new vscode.Position(end, 0));
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,16 +35,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "restructuredtables" is now active!');
+	console.log('Extension "restructuredtables" is active.');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('restructuredtables.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
+	let disposable = vscode.commands.registerCommand('restructuredtables.gridTableToListTable', () => {
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from restructuredTables!');
+		vscode.window.showInformationMessage('restructuredTables received "gridTableToListTable".');
+
+		
 	});
 
 	context.subscriptions.push(disposable);
